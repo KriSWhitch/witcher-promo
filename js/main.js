44e5f683f64298484ab4dfda553d21927d2cf7f7
KriSWhitch <krisfragerfeed@gmail.com>
2020-08-03 09:44:29 +0300

    header + menu sections

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Mobile Menu
+    const mMenuBtn = $(".m-menu-button");
+    const mMenu = $(".m-menu");
+    mMenuBtn.on("click", function(){
+        mMenu.toggleClass("active");
+        $(".support-search-mobile").css("display", "flex");
+        $("body").toggleClass("no-scroll");
+        // scroll-fix for IPhone
+        $(document).on("touchmove",function(event){
+            event.preventDefault();
+          });
+    });
+
+    // Burger Animation
+    $(".m-menu-button").click(function () {
+        $(this).toggleClass("active");
+    });
+
+});
